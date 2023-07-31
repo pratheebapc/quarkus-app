a45fd1fb7ab167b4ac8cc99ccff69cd3c8bb77d9
Anushiya <anushiyadhamu@gmail.com>
2023-07-31 23:22:38 +0530

    added log

--- src/main/java/org/telemeter/Rest/PersonResource.java
+++ src/main/java/org/telemeter/Rest/PersonResource.java
@@ -1,6 +1,8 @@
 package org.telemeter.Rest;
 
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.telemeter.Entity.Person;
 
@@ -15,7 +17,9 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/persons")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,42 +29,82 @@ public class PersonResource {
     @Inject
     EntityManager em;
 
+    private static final Logger logger = LogManager.getLogger(PersonResource.class.getName());
+
     @GET
     public List<Person> getAllPersons() {
+        logger.info("Getting all persons.");
         return em.createQuery("SELECT p FROM Person p", Person.class).getResultList();
     }
 
     @GET
     @Path("/{id}")
-    public Person getPersonById(@PathParam("id") Long id) {
-        return em.find(Person.class, id);
+    public Response getPersonById(@PathParam("id") Long id) {
+        logger.info("Getting person by ID: " + id);
+        Person person = em.find(Person.class, id);
+        if (person != null) {
+            return Response.ok(person).build();
+        } else {
+            logger.error("Person with ID " + id + " not found.");
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 
     @POST
     @Transactional
-    public void createPerson(Person person) {
-    em.merge(person);
+    public Response createPerson(Person person) {
+        // Add input validation here if required
+        logger.info("Creating person: " + person);
+        em.merge(person);
+        return Response.status(Response.Status.CREATED).build();
     }
 
+    @POST
+    @Path("/createError")
+    @Transactional
+    public Response createPersonError(Person person) {
+        // Input validation: Check if name and age are not null, and age is positive
+        if (person.getName() == null || person.getAge() <= 0) {
+            logger.error("Invalid input for creating person: " + person);
+            throw new WebApplicationException("Invalid input for creating person.",
+                    Response.Status.INTERNAL_SERVER_ERROR);
+        }
+
+        logger.info("Creating person: " + person);
+        em.merge(person);
+        return Response.status(Response.Status.CREATED).build();
+    }
 
     @PUT
     @Path("/{id}")
     @Transactional
-    public void updatePerson(@PathParam("id") Long id, Person updatedPerson) {
+    public Response updatePerson(@PathParam("id") Long id, Person updatedPerson) {
+        logger.info("Updating person with ID: " + id);
         Person person = em.find(Person.class, id);
         if (person != null) {
+            // Add input validation for updatedPerson if required
             person.setName(updatedPerson.getName());
             person.setAge(updatedPerson.getAge());
+            em.merge(person); // Persist the changes
+            return Response.ok().build();
+        } else {
+            logger.error("Person with ID " + id + " not found.");
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
 
     @DELETE
     @Path("/{id}")
     @Transactional
-    public void deletePerson(@PathParam("id") Long id) {
+    public Response deletePerson(@PathParam("id") Long id) {
+        logger.info("Deleting person with ID: " + id);
         Person person = em.find(Person.class, id);
         if (person != null) {
             em.remove(person);
+            return Response.noContent().build();
+        } else {
+            logger.error("Person with ID " + id + " not found.");
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
 }
